ed7e75ed401235d5966ced82ee0599bf7ef3154b
Kazaz-Or <83350680+Kazaz-Or@users.noreply.github.com>
2022-11-29 16:45:48 +0200

    Launches model and GET method

--- projects/NASAProject/server/src/app.js
+++ projects/NASAProject/server/src/app.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -14,8 +16,11 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
